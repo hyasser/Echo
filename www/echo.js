d4f71986e46c9333eabddbdbb95973d9c3e9a084
hyasser <heidi.yasser@telepin.com>
2014-07-08 12:55:34 +0200

    Update echo.js

--- www/echo.js
+++ www/echo.js
@@ -1,7 +1,27 @@
-  window.echo = function(str, callback) {
-        cordova.exec(callback, function(err) {
-            callback('Nothing to echo.');
-        }, "Echo", "echo", [str]);
-    };
-    
-    
+/**
+ * @constructor
+ */
+var Echo = function() {
+};
+
+Echo.prototype.echo = function(str, callback) {
+
+	cordova.exec(callback, function(err) {
+		callback('Nothing to echo.');
+	}, "Echo", "echo", [ str ]);
+
+	// cordova.exec(callback, callback, "Echo", "echo", [str]);
+};
+
+// Plug in to Cordova
+cordova.addConstructor(function() {
+
+	if (!window.Cordova) {
+		window.Cordova = cordova;
+	}
+	;
+
+	if (!window.plugins)
+		window.plugins = {};
+	window.plugins.Echo = new Echo();
+});
